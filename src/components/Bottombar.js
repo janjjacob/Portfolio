e1e9d9b9a0149cae135d7d84f1346e8e11fabe31
janjjacob <janjjacob@gmail.com>
2021-09-15 01:33:35 -0400

    first commit

---
+++ src/components/Bottombar.js
@@ -0,0 +1,29 @@
+import React from "react";
+
+export default function Bottombar() {
+  return (
+    <header className="bg-blue-800 md:sticky top-0 z-10">
+      <div className="container mx-auto flex flex-wrap p-5 flex-col md:flex-row items-center">
+        <a className="title-font font-medium text-white mb-4 md:mb-0">
+          <a href="https://github.com/janjjacob">
+            <img
+              className="object-cover object-center rounded"
+              src="./githubLogo.svg"
+            />
+          </a>
+          <a href="https://www.linkedin.com/in/jan-jacob-955028203/">
+            <img
+              className="object-cover object-center rounded"
+              src="./linkedinLogo.svg"
+            />
+          </a>
+        </a>
+        <nav className="md:mr-auto md:ml-4 md:py-1 md:pl-4 md:border-l md:border-gray-700 flex flex-wrap items-center text-base justify-center"></nav>
+        <a className="inline-flex mr-5 text-white text-lg">
+          jacob.jan@northeastern.edu
+        </a>
+        <a className="inline-flex mr-5 text-white text-lg">(508) 210-6720</a>
+      </div>
+    </header>
+  );
+}
---
+++ src/data.js
@@ -0,0 +1,49 @@
+export const projects = [
+  {
+    title: "Image Processing",
+    languages: "Java/Swing",
+    description:
+      "Interactive GUI that loads individual or layers of images and can perform transformations, such as grey-scale, sepia-tone, blur, sharpen. These images can then be saved with the transformations. (Available on Request)",
+    image: "./project-1.gif",
+    link: "https://github.com/sidg1215/Image-Processing-Application",
+  },
+  {
+    title: "Maze Solver",
+    languages: "Java",
+    description:
+      "Generates a random maze of given size (up to 100x60) and generates an interactive GUI that can be solved using arrow keys or solved using a visualized depth-first or breadth-first search when prompted. (Available on Request)",
+    image: "./project-2.gif",
+    link: "https://github.com/janjjacob/Maze-Solver",
+  },
+  {
+    title: "Connectwork",
+    languages: "React Native, Expo, Firebase",
+    description:
+      "App that faciliates communication between students and instructors to set up events. Currently allows creation of student and instructor accounts, editing accounts, and connecting with other accounts. (In Progress)",
+    image: "./project-3.png",
+    link: "https://github.com/janjjacob/Connectwork",
+  },
+  {
+    title: "Data Analysis of Crop Production",
+    languages: "Pandas, Matplotlib, Seaborn",
+    description:
+      "Analyzed data set of the potential impacts of climate change on world food supply to determine trends in crop production based on changes in atmospheric composition based on equilibrium or transient situations",
+    image: "./project-4.png",
+    link: "https://github.com/janjjacob/Snowball-Project",
+  },
+];
+
+export const skills = [
+  "Java",
+  "Python",
+  "JavaScript",
+  "C",
+  "React.js",
+  "node.js",
+  "NumPy",
+  "MATLAB",
+  "CSS",
+  "HTML",
+  "Tableau",
+  "Bootstrap",
+];
